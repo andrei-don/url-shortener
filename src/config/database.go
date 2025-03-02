f4c04dc474828464c35c3c0277120c2486c552e1
andrei-don <alexandreistan.98@gmail.com>
2025-03-02 16:32:40 +0000

    (feat) Added helm chart to deploy the application
    This commit adds the helm chart to deploy the application. Helm chart dependencies were added for nginx ingress controller and kube-prometheus-stack which can be disabled based on Helm chart values.
    
    The application code was modified to support database connection retries and prevent the pod from crashing on startup. An additional healthz endpoint was added to test connections to Redis and Postgres. This endpoint is used by the ReadinessProbe to check if the application pod can connect to its dependencies before marking the container as 'Ready'.
    
    In order to pass the postgres password to the database and to the application, we are making use of the helm-secrets plugin which decrypts secrets on the fly during the helm release installation.

--- src/config/database.go
+++ src/config/database.go
@@ -3,22 +3,33 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"log"
+
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
 var sqlOpen = sql.Open
 
-func ConnectDatabase(dsn string) (*sql.DB, error) {
-	db, err := sqlOpen("postgres", dsn)
-	if err != nil {
-		return nil, err
-	}
+func ConnectDatabase(dsn string, maxRetries int, retryInterval time.Duration) (*sql.DB, error) {
 
-	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("database is not reachable: %v", err)
-	}
+	var db *sql.DB
+	var err error
+	for i := 0; i < maxRetries; i++ {
+		db, err = sqlOpen("postgres", dsn)
+		if err != nil {
+			log.Printf("Failed to open database connection (attempt %d/%d): %v", i+1, maxRetries, err)
+			time.Sleep(retryInterval)
+			continue
+		}
 
-	fmt.Println("Successfully connected!")
-	return db, nil
+		if err = db.Ping(); err == nil {
+			fmt.Println("Successfully connected!")
+			return db, nil
+		}
+		log.Printf("Failed to ping database (attempt %d/%d): %v", i+1, maxRetries, err)
+		time.Sleep(retryInterval)
+	}
+	return nil, fmt.Errorf("could not connect to database after %d retries: %v", maxRetries, err)
 }
